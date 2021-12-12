09ffb0ab5f6add9ec9b62f00ffbeaf59715f316e
smallpp420 <78730004+SmallPP420@users.noreply.github.com>
2021-12-12 17:48:03 +0000

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_WSP_sprout-user
-add_lunch_combo omni_WSP_sprout-userdebug
-add_lunch_combo omni_WSP_sprout-eng
+add_lunch_combo twrp_WSP_sprout-user
+add_lunch_combo twrp_WSP_sprout-userdebug
+add_lunch_combo twrp_WSP_sprout-eng
